66d215e638ae709f838d042d1b8699bf07f584fa
mschauer <moritzschauer@web.de>
2015-07-16 21:23:10 +0200

    Fix example.

--- examples/bio.jl
+++ examples/bio.jl
@@ -1,4 +1,4 @@
-include("idr.jl")
+using IDRs
 n = 20
 b = 20
 A = rand(n,n)
@@ -8,7 +8,7 @@ C = Xsol + A*Xsol*B
 X0 = rand(n,b)
 maxit = n*n*n
 tol = eps()
-s = 8                     
+s = 32                     
 
-@time X, RES = stein(A,B, C,s,tol, maxit)
+@time X = stein(A,B, C,s,tol, maxit)
 println(vecnorm(Xsol - X))
