5445fc7b4e0320d529b10e6288964c894a641de9
mschauer <moritzschauer@web.de>
2015-07-18 14:16:30 +0200

    Test stein and syl.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,21 @@
 using IDRsSolver
 using Base.Test
 
+srand(55)
 n = 10
 b = 10
 A = rand(n,n)
 B = rand(b,n)
 Xsol = rand(n,b)
 C = Xsol + A*Xsol*B
-X0 = rand(n,b)
-maxit = n*n*n
-tol = 1E-10
-s = 10
+D = A*Xsol + Xsol*B
 
-X = stein(A,B, C,s,tol, maxit)
-@test vecnorm(Xsol - X) <= tol
+maxit = (n*b)^2
+tol = 1E-8
+s = 8
+
+X = stein(A,B, C, s, tol, maxit)
+@test vecnorm(Xsol - X) <= 2tol*vecnorm(C)
+
+X = syl(A,B, D, s, tol, maxit)
+@test vecnorm(D - A*X-X*B) <= tol*vecnorm(D)
